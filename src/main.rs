21363b2d4ee03ef16e30f98a6f21c2ae6848f46c
Eric Price <eric@navier.com>
2024-05-15 20:51:47 -0400

    lesson 10 completed

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 use std::io::Read;
 
-fn read_version(transaction_hex: &str) -> u32 {
+fn read_version(transaction_bytes: &mut &[u8]) -> u32 {
+    let mut buffer = [0; 4];
+    transaction_bytes.read(&mut buffer).unwrap();
+
+    u32::from_le_bytes(buffer)
+}
+
+fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
+    let mut compact_size = [0; 1];
+    transaction_bytes.read(&mut compact_size).unwrap();
+
+    // using a ref prevents making a copy, i would think
+    // and deref trait lets it be used for pattern matching
+    let first_byte = compact_size[0];
+    match first_byte {
+        (1..=252) => compact_size[0] as u64,
+        253 => {
+            let mut buffer = [0; 2];
+            transaction_bytes.read(&mut buffer).unwrap();
+            let extra_bytes = u16::from_le_bytes(buffer);
+            first_byte as u64 + extra_bytes as u64
+        }
+        254 => {
+            let mut buffer = [0; 4];
+            transaction_bytes.read(&mut buffer).unwrap();
+            let extra_bytes = u32::from_le_bytes(buffer);
+            first_byte as u64 + extra_bytes as u64
+        }
+        255 => {
+            let mut buffer = [0; 8];
+            transaction_bytes.read(&mut buffer).unwrap();
+            let extra_bytes = u64::from_le_bytes(buffer);
+            first_byte as u64 + extra_bytes
+        }
+        _ => panic!("invalid compact size"),
+    }
+}
+fn main() {
+    let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let mut buffer = [0; 4];
-    bytes_slice.read(&mut buffer).unwrap();
-    u32::from_le_bytes(buffer)
-}
-fn main() {
-    let version = read_version("010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000");
+    let version = read_version(&mut bytes_slice);
+    let input_length = read_compact_size(&mut bytes_slice);
+
     println!("Version: {}", version);
+    println!("Input Length: {}", input_length);
 }
