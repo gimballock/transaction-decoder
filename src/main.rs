5be09a5dd497614c933a4b4c521301521c56ab9c
Eric Price <eric@navier.com>
2024-05-20 20:20:11 -0400

    lesson 12

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Read;
 
-fn read_version(transaction_bytes: &mut &[u8]) -> u32 {
+fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
     let mut buffer = [0; 4];
     transaction_bytes.read(&mut buffer).unwrap();
 
@@ -32,13 +32,34 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
         _ => panic!("invalid compact size"),
     }
 }
+
+fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
+    let mut buffer = [0; 32];
+    transaction_bytes.read(&mut buffer).unwrap();
+    buffer.reverse();
+    buffer
+}
+
+fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
+    let script_size = read_compact_size(transaction_bytes) as usize;
+    let mut buffer = vec![0; script_size];
+    transaction_bytes.read(&mut buffer).unwrap();
+    buffer
+}
 fn main() {
     let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let version = read_version(&mut bytes_slice);
+    let version = read_u32(&mut bytes_slice);
     let input_length = read_compact_size(&mut bytes_slice);
+
+    for _ in 0..input_length {
+        let txid = read_txid(&mut bytes_slice);
+        let output_index = read_u32(&mut bytes_slice);
+        let script = read_script(&mut bytes_slice);
+        let sequence = read_u32(&mut bytes_slice);
+    }
 
     println!("Version: {}", version);
     println!("Input Length: {}", input_length);
@@ -70,7 +91,7 @@ mod unit_tests {
         // fd is 253
         // transaction has 20,000 empty inputs
         let hex = "fd204e";
-        let decoded = hex::decode(transaction_hex).unwrap();
+        let decoded = hex::decode(hex).unwrap();
         let mut bytes = decoded.as_slice();
         let length = read_compact_size(&mut bytes);
         let expected_length = 20_000_u64;
