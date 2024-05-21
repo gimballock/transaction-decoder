3b4c57b607550f42fb146193030c5199494ac721
Eric Price <eric@navier.com>
2024-05-20 23:27:17 -0400

    lesson 14

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use serde::Serialize;
 use std::io::Read;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Input {
-    txid: [u8; 32],
+    txid: String,
     output_index: u32,
-    script: Vec<u8>,
+    script: String,
     sequence: u32,
+}
+
+#[derive(Debug, Serialize)]
+struct Transaction {
+    version: u32,
+    inputs: Vec<Input>,
 }
 
 fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
@@ -41,18 +48,18 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     }
 }
 
-fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
+fn read_txid(transaction_bytes: &mut &[u8]) -> String {
     let mut buffer = [0; 32];
     transaction_bytes.read(&mut buffer).unwrap();
     buffer.reverse();
-    buffer
+    hex::encode(buffer)
 }
 
-fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
+fn read_script(transaction_bytes: &mut &[u8]) -> String {
     let script_size = read_compact_size(transaction_bytes) as usize;
     let mut buffer = vec![0; script_size];
     transaction_bytes.read(&mut buffer).unwrap();
-    buffer
+    hex::encode(buffer)
 }
 fn main() {
     let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
@@ -63,7 +70,6 @@ fn main() {
     let input_length = read_compact_size(&mut bytes_slice);
 
     let mut inputs = vec![];
-
     for _ in 0..input_length {
         let txid = read_txid(&mut bytes_slice);
         let output_index = read_u32(&mut bytes_slice);
@@ -78,9 +84,12 @@ fn main() {
         });
     }
 
-    println!("Version: {}", version);
-    println!("Input Length: {}", input_length);
-    println!("Inputs: {:?}", inputs);
+    let txn = Transaction { version, inputs };
+
+    println!(
+        "Transaction: {}",
+        serde_json::to_string_pretty(&txn).unwrap()
+    );
 }
 
 #[cfg(test)]
